fd2ebe20397fbf201f6e7430dec385df6d7c769d
Bhavana4c621210 <bhavana012phb@gmail.com>
2026-06-14 23:14:31 +0530

    Add files via upload

---
+++ DEEPSKILLING/module1/exercise1_Singleton Pattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using CognizantDN5.DesignPatterns;
+
+DocumentFactory wordFactory = new WordFactory();
+IDocument wordDoc = wordFactory.CreateDocument();
+wordDoc.Open();
+
+DocumentFactory pdfFactory = new PdfFactory();
+IDocument pdfDoc = pdfFactory.CreateDocument();
+pdfDoc.Open();
+
+DocumentFactory excelFactory = new ExcelFactory();
+IDocument excelDoc = excelFactory.CreateDocument();
+excelDoc.Open();
+
+Console.ReadLine();
---
+++ DEEPSKILLING/module1/exercise1_Singleton Pattern/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CognizantDN5.DesignPatterns
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+
+        public void ShowMessage()
+        {
+            Console.WriteLine("Singleton Object Created");
+        }
+    }
+}
