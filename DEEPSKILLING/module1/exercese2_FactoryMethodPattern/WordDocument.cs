3c7ff7f977809847a6d97ae19ccae85808b6fa7c
Bhavana4c621210 <bhavana012phb@gmail.com>
2026-06-14 23:55:18 +0530

    Add files via upload

---
+++ DEEPSKILLING/module1/exercese2_FactoryMethodPattern/WordDocument.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CognizantDN5.DesignPatterns
+{
+    public class WordDocument : IDocument
+    {
+        public void Open()
+        {
+            Console.WriteLine("Word Document Opened");
+        }
+    }
+}
